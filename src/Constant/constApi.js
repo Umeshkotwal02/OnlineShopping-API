b7ef59901c262c2bde50efccc3b7cca884f07d03
Kotwal Umesh <umeshkotwal658@gmail.com>
2025-01-22 10:21:40 +0530

    Update constApi.js
    add new url

--- src/Constant/constApi.js
+++ src/Constant/constApi.js
@@ -1,2 +1,2 @@
 // export const API_URL = "https://kapoorlehengasaree.com/admin/"
-export const API_URL = "https://project.sridixtechnology.com/Kapoor/"
+export const API_URL = "https://project.sridixtechnology.com/Kapoor/api/"
